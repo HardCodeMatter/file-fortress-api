36404a80082baaccf4c8a2e89fb8203c31962427
HardCodeMatter <dklymovets1@gmail.com>
2024-08-14 17:50:13 +0300

    Add alembic config and User model
    Added alembic basic configuration with black format. Created User model.

---
+++ src/auth/models.py
@@ -0,0 +1,23 @@
+from sqlalchemy.orm import Mapped, mapped_column
+
+from src.database import Base
+from src.models import IDMixin, TimestampMixin
+
+
+class User(IDMixin, TimestampMixin, Base):
+    __tablename__ = 'users'
+
+    username: Mapped[str] = mapped_column(unique=True, index=True, nullable=False)
+    email: Mapped[str] = mapped_column(unique=True, index=True, nullable=False)
+    first_name: Mapped[str]
+    last_name: Mapped[str]
+
+    is_active: Mapped[bool] = mapped_column(default=True)
+    is_verified: Mapped[bool] = mapped_column(default=False)
+    is_superuser: Mapped[bool] = mapped_column(default=False)
+
+    def __str__(self) -> str:
+        return f'User(id={self.id}, username={self.username}, is_superuser={self.is_superuser})'
+
+    def __repr__(self) -> str:
+        return self.__str__
